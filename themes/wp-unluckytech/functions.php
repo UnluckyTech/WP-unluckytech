91bab7aa98b954a313d649be30fd07794c4d6d54
UnluckyTech <github@stawse.simplelogin.com>
2024-05-17 21:08:26 -0400

    idk mate

--- themes/wp-unluckytech/functions.php
+++ themes/wp-unluckytech/functions.php
@@ -47,3 +47,9 @@ function custom_excerpt_lenght($length) {
 }
 add_filter('excerpt_length', 'custom_excerpt_lenght');
 
+// Enqueue Particle Animation JavaScript and CSS
+function enqueue_particle_animation_assets() {
+    wp_enqueue_style('particle-animation-css', get_template_directory_uri() . '/assets/css/particle-animation.css');
+    wp_enqueue_script('particle-animation-js', get_template_directory_uri() . '/assets/js/particle-animation.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_particle_animation_assets');
