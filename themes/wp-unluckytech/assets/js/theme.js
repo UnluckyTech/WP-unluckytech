8a1ed350bfdb57ce1634e5b3f307ea77e8c65323
UnluckyTech <github@stawse.simplelogin.com>
2024-05-24 17:31:55 -0400

    footer

---
+++ themes/wp-unluckytech/assets/js/theme.js
@@ -0,0 +1,19 @@
+function toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+    let icon = document.querySelector('.theme-toggle i');
+    if (document.body.classList.contains('dark-theme')) {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }
+}
+
+// Apply dark theme by default
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.add('dark-theme');
+    let icon = document.querySelector('.theme-toggle i');
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+});
