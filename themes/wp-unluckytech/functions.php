09f4e065edec17b0dccca1525a2fc949ce9dc474
UnluckyTech <github@stawse.simplelogin.com>
2024-06-02 15:14:07 -0400

    Update functions.php

--- themes/wp-unluckytech/functions.php
+++ themes/wp-unluckytech/functions.php
@@ -1,6 +1,37 @@
 <?php
 
+if ( ! function_exists( 'unlucky_theme_support' ) ) :
 
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * @since My theme name 1.0
+     *
+     * @return void
+     */
+
+endif;
+add_action( 'after_setup_theme', 'unlucky_theme_support' );
+
+/*-----------------------------------------------------------
+Enqueue Styles
+------------------------------------------------------------*/
+
+if ( ! function_exists( 'unlucky_styles' ) ) :
+
+    function unlucky_styles() {
+        // Register Stylesheet
+        wp_enqueue_style('unlucky-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version'));
+        wp_enqueue_style('unlucky-style-blocks', get_template_directory_uri() . '/assets/css/blocks.css');
+
+        // Enqueue Font Awesome
+        wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
+
+    }
+
+endif;
+
+add_action( 'wp_enqueue_scripts', 'unlucky_styles' );
 
 /*-----------------------------------------------------------
 Customising The Excerpt Length
