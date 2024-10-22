5f2afc711bf46525e63a78154c5f702b5d60f0b2
UnluckyTech <github@stawse.simplelogin.com>
2024-10-22 12:47:17 -0400

    Update categories.php

--- themes/wp-unluckytech/patterns/categories.php
+++ themes/wp-unluckytech/patterns/categories.php
@@ -13,7 +13,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Categories</title>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/categories.css">
 </head>
 <body>
 
